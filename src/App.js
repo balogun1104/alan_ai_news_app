dd5ace5dfc9efa2bd12a1dea12e9f8e6b9fb3fda
Balogun Razak <razak2free@gmail.com>
2022-10-31 14:21:42 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,19 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import alanBtn from '@alan-ai/alan-sdk-web';
+
+const alanKey= 'd48aa70c170e4ecf62fc3896509ae9b52e956eca572e1d8b807a3e2338fdd0dc/stage';
 
 const App = () => {
+    useEffect(() => {
+        alanBtn({
+            key: alanKey,
+            onCommand: ({ command, articles }) => {
+                if(command === 'newHeadlines') {
+                   console.log(articles);
+                }
+            }
+        })
+    }, [])
   return (
     <div>
       <h1>Alan Ai new app</h1>
